5a96d217afeb198c593b3012f5b993632a225f81
Apagyi Dávid <apagyi.david@gmail.com>
2020-04-18 19:20:27 +0200

    Updates

---
+++ precache-manifest.9305650de8bb0d988fea09e18c85941a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cfe12187333527e610475c95b2623bd",
+    "url": "/elelmezesi-terv/index.html"
+  },
+  {
+    "revision": "43f1023516350413e750",
+    "url": "/elelmezesi-terv/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "067bd7fe6a7b31c15bcf",
+    "url": "/elelmezesi-terv/static/css/main.e9b977cf.chunk.css"
+  },
+  {
+    "revision": "43f1023516350413e750",
+    "url": "/elelmezesi-terv/static/js/2.c0de60ca.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/elelmezesi-terv/static/js/2.c0de60ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067bd7fe6a7b31c15bcf",
+    "url": "/elelmezesi-terv/static/js/main.dddf95b7.chunk.js"
+  },
+  {
+    "revision": "d44254a3f257e15a0123",
+    "url": "/elelmezesi-terv/static/js/runtime-main.567b81b0.js"
+  },
+  {
+    "revision": "36606a997a6a1e7ad818327467193ba6",
+    "url": "/elelmezesi-terv/static/media/footer_nyakkendő.36606a99.svg"
+  }
+]);
